1245fce78a204a81b99a8f5bd160b580f969418f
Denis <gvvvvf@gmail.com>
2022-08-13 16:53:05 +0200

    feat: add page not found

--- src/components/page-not-found/page-not-found.tsx
+++ src/components/page-not-found/page-not-found.tsx
@@ -1,20 +1,19 @@
-import { Component, Host, getAssetPath, h } from '@stencil/core';
+import { Component, Host, getAssetPath, h } from "@stencil/core";
 
 @Component({
-  tag: 'page-not-found',
-  styleUrl: 'page-not-found.scss',
-  assetsDirs: ['assets']
+  tag: "page-not-found",
+  styleUrl: "page-not-found.scss",
+  assetsDirs: ["assets"],
 })
 export class PageNotFound {
   render() {
     return (
       <Host>
-        <div class='error'>
-          <img src={getAssetPath(`./assets/stop_man.svg`)} />
-          <div class='error-message'>404 PAGE NOT FOUND</div>
+        <div class="error">
+          <img src={getAssetPath("assets/stop-man.svg")} />
+          <div class="error-message">404 PAGE NOT FOUND</div>
         </div>
       </Host>
     );
   }
-
 }
