7740d6033f90de56be6f5f8c882cffe2a1e5a26d
Denis <gvvvvf@gmail.com>
2022-08-14 19:36:31 +0200

    feat: Created a navigation with SCSS

--- src/components/app-navigation/app-navigation.tsx
+++ src/components/app-navigation/app-navigation.tsx
@@ -1,13 +1,12 @@
-import { Component, Host, h, Prop } from '@stencil/core';
-import { injectHistory, RouterHistory } from '@stencil/router';
+import { Component, Host, h, Prop } from "@stencil/core";
+import { injectHistory, RouterHistory } from "@stencil/router";
 
 @Component({
-  tag: 'app-navigation',
-  styleUrl: 'app-navigation.css',
+  tag: "app-navigation",
+  styleUrl: "app-navigation.scss",
   shadow: true,
 })
 export class AppNavigation {
-
   @Prop() history: RouterHistory;
 
   navigateTo(path: string) {
@@ -17,15 +16,51 @@ export class AppNavigation {
   render() {
     return (
       <Host>
-        <ul>
-          <li onClick={() => { this.navigateTo('/') }}>Home</li>
-          <li onClick={() => { this.navigateTo('/list') }}>List</li>
-          <li onClick={() => { this.navigateTo('/details') }}>Details</li>
-        </ul>
+        {
+          <div class="hamburger-menu">
+            <input class="menu__toggle" id="menu__toggle" type="checkbox" />
+            <label class="menu__btn" htmlFor="menu__toggle">
+              <span></span>
+            </label>
+            <ul class="menu__box">
+              <li
+                class="menu__item"
+                onClick={() => {
+                  this.navigateTo("/");
+                }}
+              >
+                Home
+              </li>
+              <li
+                class="menu__item"
+                onClick={() => {
+                  this.navigateTo("/list");
+                }}
+              >
+                List
+              </li>
+              <li
+                class="menu__item"
+                onClick={() => {
+                  this.navigateTo("/details");
+                }}
+              >
+                Details
+              </li>
+              <li
+                class="menu__item"
+                onClick={() => {
+                  this.navigateTo("/contact");
+                }}
+              >
+                Contact
+              </li>
+            </ul>
+          </div>
+        }
       </Host>
     );
   }
-
 }
 
 injectHistory(AppNavigation);
